cfa89d196d28eb0fa8aab353e5f5baf7b2abc5c0
achetverikov <stop.psy@gmail.com>
2017-07-18 09:26:36 +0200

    plyr::rename instead of rename

--- apastats/R/summarySE.R
+++ apastats/R/summarySE.R
@@ -28,7 +28,7 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
   )
 
   # Rename the "mean" column
-  datac <- rename(datac, c("mean" = measurevar))
+  datac <- plyr::rename(datac, c("mean" = measurevar))
 
   datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
 
